9b8f7d50bdc8a986a982b39902b9df63ec560c9e
Manas Agrawal <agrawal.manas31@gmail.com>
2025-11-02 15:20:05 -0500

    Add a basic REST endpoint to StockSproutApplication.

--- backend/stocksprout/src/main/java/com/vampz/stocksprout/StocksproutApplication.java
+++ backend/stocksprout/src/main/java/com/vampz/stocksprout/StocksproutApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class StocksproutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StocksproutApplication.class, args);
 	}
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
 
 }

5b2e239f8440dde2229e2fb05d9216ffb4e48c22
Manas Agrawal <agrawal.manas31@gmail.com>
2025-11-03 02:24:38 -0500

    Add http request file, dependencies.

--- backend/stocksprout/src/main/java/com/vampz/stocksprout/StocksproutApplication.java
+++ backend/stocksprout/src/main/java/com/vampz/stocksprout/StocksproutApplication.java
@@ -2,18 +2,15 @@ package com.vampz.stocksprout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class StocksproutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StocksproutApplication.class, args);
 	}
-
-    @GetMapping
-    public String hello() {
-        return "Hello World!";
-    }
 
 }

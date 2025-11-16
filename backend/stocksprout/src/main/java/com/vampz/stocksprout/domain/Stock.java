9eafaa2aa645479573327199a5e0a04be47317d8
hellloaniketsaini <108158244+hellloaniketsaini@users.noreply.github.com>
2025-11-15 22:01:37 -0500

    Epic 3 US006 implemented stock class

--- backend/stocksprout/src/main/java/com/vampz/stocksprout/domain/Stock.java
+++ backend/stocksprout/src/main/java/com/vampz/stocksprout/domain/Stock.java
@@ -1,5 +1,42 @@
 package com.vampz.stocksprout.domain;
 
+import lombok.*;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Stock {
+
+
+    /**
+     *Generate primary key for stock database
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Added columns for database 
+     * cannot be Null and every value should be unique
+     */
+    @Column(nullable = false, unique = true)
+    private String symbol;
+
+    private String name;
+    private String exchange;
+    private String sector;
+
+    /**
+     * Connecting stock to stock price database
+     */
+    @OneToMany(mappedBy = "stock", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StockPrice> prices = new ArrayList<>();
 
 }
